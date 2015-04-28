200d7a59be1fd5926189f0e491541b723b7a76e1
state-hiu <HIU_INFO@state.gov>
2015-04-28 00:20:38 -0400

    various fixes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,12 @@
 import os
 
 bind = 'unix:///tmp/gunicorn.sock'
-workers = 4
+workers = 5
 worker_class = 'gevent'
 #worker_class = 'egg:gunicorn#gevent'
 # Logging
-loglevel = 'info'
+loglevel = 'critical'
 acces_logfile = "access.log"
 error_logfile = "error.log"
-enable_stdio_inheritance = True
+#enable_stdio_inheritance = True
 timeout = 360
